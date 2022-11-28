086cb41cbd525463a608f69b40be208f2750c6a9
Ahmed Gouda <ahmedmosad405@gmail.com>
2022-11-29 01:42:10 +0200

    Writing Multiply and PrintingData funcs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿Console.Write("Enter rows: ");
+﻿Console.WriteLine("Welcome to matrix-divider.\nWe will divide two matrices you choose.");
+
+Console.Write("Enter rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter columns: ");
 int cols = Convert.ToInt32(Console.ReadLine());
-
+if (rows != cols)
+{
+    Console.WriteLine("Collumns and rows must be equal.");
+    return;
+}
 // Initializing arrays
 double [,] arr1 = new double [rows, cols];
 double [,] arr2 = new double [rows, cols];
 double [,] unity = new double [rows, cols];
+double [,] output = new double [rows, cols];
 
 // Calling functions
 GettingData(ref arr1, "first");
 GettingData(ref arr2, "second");
 MakeUnity(ref unity);
 Inverse(ref arr2);
+Multiply(ref arr1, ref arr2, ref output);
+PrintingData(ref output);
 
 // Reading the matrices from the user
 void GettingData(ref double [,] arr, string order)
@@ -30,7 +39,6 @@ void GettingData(ref double [,] arr, string order)
 }
 
 // Creating the unity matrix
-
 void MakeUnity(ref double [,] arr)
 {
     for (int i = 0; i < rows; i++)
@@ -64,6 +72,7 @@ void Inverse(ref double [,] arr)
             mixedArr[i, k] = unity[i, k - cols];
         }
     }
+
     // Doing gauss-jordan elimination
     void elimination(int type, int i, double multiply)
     {
@@ -112,15 +121,47 @@ void Inverse(ref double [,] arr)
             // We divide by it, to convert the diagonal elements to ones.
             // So, we get the unity matrix in the left side of the mixedArr
             // therefore, we get the inverse of the matrix successfully.
-            arr[i, j - cols] = Math.Round(mixedArr[i, j] / mixedArr[i, i], 3);
+            arr[i, j - cols] = mixedArr[i, j] / mixedArr[i, i];
         }
     }
 }
-for (int i = 0; i < rows; i++)
+
+// multiplying the first array by the inversed array
+void Multiply(ref double [,] matrix1, ref double [,] matrix2, ref double [,] result)
 {
-    for (int j = 0; j < cols; j++)
+    // Math rules
+    int resultRows = matrix1.GetLength(0);
+    int resultCols = matrix2.GetLength(1);
+    result = new double [resultRows, resultCols];
+    double sum;
+
+    for (int k = 0; k < resultCols; k++)
     {
-        Console.Write($"{arr2[i, j]}\t");
+        
+        for (int i = 0; i < resultRows; i++)
+        {
+            sum = 0;
+            for (int j = 0; j < resultCols; j++)
+            {
+                sum += matrix1[i, j] * matrix2[j, k];
+            }
+            result[i, k] = sum;
+        }
     }
-    Console.WriteLine();
+}
+
+// Now the final step
+// Printing the result
+void PrintingData(ref double [,] result)
+{
+    Console.WriteLine("---The result matrix---");
+    for (int i = 0, resultRows = result.GetLength(0); i < resultRows; i++)
+    {
+        for (int j = 0, resultCols = result.GetLength(1); j < resultCols; j++)
+        {
+            string messeage = String.Format("{0,8}\t", result[i, j]);
+            Console.Write(messeage);
+        }
+        Console.WriteLine();
+    }
 }
